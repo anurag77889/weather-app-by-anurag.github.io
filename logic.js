854f0911dc2077df3e03dc49c9f1775357a2d97b
Anurag Swarnakar <81405295+anurag77889@users.noreply.github.com>
2024-09-24 23:34:31 +0530

    Create logic.js

---
+++ logic.js
@@ -0,0 +1,67 @@
+const apiKey = "b1c97555e43672dd23c7e6c5d9b6d1c1";
+const apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".searchbox input");
+const searchBtn = document.querySelector(".searchbox button");
+
+async function checkWeather(city) {
+  const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+
+  if (response.status == 404) {
+    document.querySelector(".error").style.display = "block";
+    document.querySelector(".weather").style.display = "none";
+  } else {
+    var data = await response.json();
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temperature").innerHTML =
+      Math.round(data.main.temp) + " °C";
+    document.querySelector(".humidity").innerHTML =
+      "Humidity: " + data.main.humidity + " %";
+    document.querySelector(".wind").innerHTML =
+      "Wind Speed: " + data.wind.speed + " km/h";
+
+    document.querySelector(".weather").style.display = "block";
+    document.querySelector(".error").style.display = "none";
+
+    if (data.main.temp >= 40 && data.main.temp <= 50) {
+      document.body.style.backgroundColor = "#E75A2D";
+      document.querySelector(".button").style.color = "#E75A2D";
+      document.querySelector(".image img").src = "/veryhot.png";
+    }
+    if (data.main.temp >= 30 && data.main.temp < 40) {
+      document.body.style.backgroundColor = "#FE981B";
+      document.querySelector(".searchbox button").style.color = "#FE981B";
+      document.querySelector(".image img").src = "/hot.png";
+    }
+    if (data.main.temp >= 20 && data.main.temp < 30) {
+      document.body.style.backgroundColor = "#FCE7A4";
+      document.querySelector(".searchbox button").style.color = "#FCE7A4";
+      document.querySelector(".image img").src = "/warm.png";
+    }
+    if (data.main.temp >= 10 && data.main.temp < 20) {
+      document.body.style.backgroundColor = "#9FDFCE";
+      document.querySelector(".searchbox button").style.color = "#9FDFCE";
+      document.querySelector(".image img").src = "/cold.png";
+    }
+
+    if (data.main.temp >= 0 && data.main.temp < 10) {
+      document.body.style.backgroundColor = "#9AD1D8";
+      document.querySelector(".searchbox button").style.color = "#9AD1D8";
+      document.querySelector(".image img").src = "/verycold.png";
+    }
+  }
+}
+
+searchBox.addEventListener("keyup", (event) => {
+  if (event.key === "Enter") {
+    document.querySelector(".headline").style.display = "none";
+    checkWeather(searchBox.value);
+  }
+});
+
+searchBtn.addEventListener("click", () => {
+  document.querySelector(".headline").style.display = "none";
+  checkWeather(searchBox.value);
+});
