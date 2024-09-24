2adfcfe499a67dc40964739c08edebff6431e523
Anurag Swarnakar <81405295+anurag77889@users.noreply.github.com>
2024-09-24 23:41:32 +0530

    Update logic.js

--- logic.js
+++ logic.js
@@ -28,28 +28,28 @@ async function checkWeather(city) {
     if (data.main.temp >= 40 && data.main.temp <= 50) {
       document.body.style.backgroundColor = "#E75A2D";
       document.querySelector(".button").style.color = "#E75A2D";
-      document.querySelector(".image img").src = "/veryhot.png";
+      document.querySelector(".image img").src = "veryhot.png";
     }
     if (data.main.temp >= 30 && data.main.temp < 40) {
       document.body.style.backgroundColor = "#FE981B";
       document.querySelector(".searchbox button").style.color = "#FE981B";
-      document.querySelector(".image img").src = "/hot.png";
+      document.querySelector(".image img").src = "hot.png";
     }
     if (data.main.temp >= 20 && data.main.temp < 30) {
       document.body.style.backgroundColor = "#FCE7A4";
       document.querySelector(".searchbox button").style.color = "#FCE7A4";
-      document.querySelector(".image img").src = "/warm.png";
+      document.querySelector(".image img").src = "warm.png";
     }
     if (data.main.temp >= 10 && data.main.temp < 20) {
       document.body.style.backgroundColor = "#9FDFCE";
       document.querySelector(".searchbox button").style.color = "#9FDFCE";
-      document.querySelector(".image img").src = "/cold.png";
+      document.querySelector(".image img").src = "cold.png";
     }
 
     if (data.main.temp >= 0 && data.main.temp < 10) {
       document.body.style.backgroundColor = "#9AD1D8";
       document.querySelector(".searchbox button").style.color = "#9AD1D8";
-      document.querySelector(".image img").src = "/verycold.png";
+      document.querySelector(".image img").src = "verycold.png";
     }
   }
 }
